6c2c8d92160be631009510b09bcc39b78aa7cdbf
Victor Avila <avilavictor1570@gmail.com>
2020-07-09 15:44:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,6 @@
 def speak_to_grandma (phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "WHAT DID YOU EAT TODAY?"
-    puts "HUH?! SPEAK UP, SONNY!"
-  else phrase == "WHAT?"
-    puts "NO, NOT SINCE 1938!"
-  end
+  if phrase == phrase.downcase
+    return "HUH?! SPEAK UP, SONNY!"
+    elsif
+    end
 end
