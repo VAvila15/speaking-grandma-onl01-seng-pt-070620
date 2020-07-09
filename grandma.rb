a376aeeaf51c330ff4ea35cd4eeec071d1059c18
Victor Avila <avilavictor1570@gmail.com>
2020-07-09 15:36:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,2 +1,9 @@
-def speak_to_grandma
-  
+def speak_to_grandma (phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == lower.case
+    puts "HUH?! SPEAK UP, SONNY!"
+  else phrase == upper.case
+    puts "NO, NOT SINCE 1938!"
+  end
+end
